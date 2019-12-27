eddb2a2e41dd62e921079577fcb0ebc8f86f938f
kushal Singh Rathore <54983775+kushalsingh-00@users.noreply.github.com>
2019-12-27 18:01:03 +0530

    #define is used for declaring constant but we also use it as function

--- constant_define.c
+++ constant_define.c
@@ -10,6 +10,6 @@ Discription  : Define As PreProcessor #define As Func
 #define add(x,y) x+y
 int main()
 {
-    printf("addition of two number is %d",add(3,4));  //it simply replace the add(x,y) with x+y
+    printf("addition of two number is %d",5*add(3,4));  //it simply replace the add(x,y) with x+y
     return 0;
 }
