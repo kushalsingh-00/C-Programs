f1b01ac5cbc4c653a92311584c50b925e4e652c8
kushal Singh Rathore <54983775+kushalsingh-00@users.noreply.github.com>
2019-12-27 21:57:11 +0530

    0 denotes octal value atthe time of initilize

---
+++ octal.c
@@ -0,0 +1,15 @@
+/*
+-------------------------------------------------------
+Name         : Octal Number
+Author       : Kushal Singh Rathore
+Discription  : Giving Input As Octal
+-------------------------------------------------------
+*/
+
+#include<stdio.h>
+int main()
+{
+    int a=052;
+    printf("%d",a);
+    return 0;
+}
