bbbcb48e88ca0b21680c74a34189d5bb7ca07f0e
kushal Singh Rathore <54983775+kushalsingh-00@users.noreply.github.com>
2019-12-27 16:34:22 +0530

    accesing time greatly reduces by using register keyword because register memory is fast

---
+++ modifier_register.c
@@ -0,0 +1,14 @@
+/*
+-------------------------------------------------------
+Name         : Register Modifier
+Author       : Kushal Singh Rathore
+Discription  : Code Illustraring The Use Of Register M.
+-------------------------------------------------------
+*/
+
+#include<stdio.h>
+int main()
+{
+    register int var;  //compiler have option for placing it 
+    return 0;
+}
