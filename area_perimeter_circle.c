5c68420495a26f0ffb70550cb7399a0e00da5d19
kushal Singh Rathore <54983775+kushalsingh-00@users.noreply.github.com>
2019-12-27 15:50:12 +0530

    FInding area and perimeter using constant in c #define

---
+++ area_perimeter_circle.c
@@ -0,0 +1,22 @@
+/*
+-------------------------------------------------------
+Name         : Area ANd Perimeter Of A Circle
+Author       : Kushal Singh Rathore
+Discription  : Code For Area And Perimeter with #define
+-------------------------------------------------------
+*/
+
+#include<stdio.h>
+#include<math.h>
+#define PIE 3.14               // constant
+int main()
+{
+    float r;
+    printf("Enter The Radius of a Circle\n");
+    scanf("%f",&r);
+    float area=PIE*pow(r,2);
+    float perimeter=2*PIE*r;
+    printf("Area of Circle is %f \nperimeter is %f",area,perimeter);
+    
+    return 0;
+}
