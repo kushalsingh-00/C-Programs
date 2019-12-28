4ac89c5f7451cad2655261c1d9e1fffabe0ec1a9
kushal Singh Rathore <54983775+kushalsingh-00@users.noreply.github.com>
2019-12-28 07:32:33 +0530

    illustrating the use of 0x

---
+++ hexadecimal.c
@@ -0,0 +1,16 @@
+/*
+-------------------------------------------------------
+Name         : Hexa Decimal
+Author       : Kushal Singh Rathore
+Discription  : Use Of Hexadecimal As Input
+-------------------------------------------------------
+*/
+
+#include<stdio.h>
+int main()
+{
+    int a=0X56A;        //  0x denotes the number is hexadecimal 
+    printf("%X",a);      //case sensative (format specifier)
+    printf("%X",a);
+    return 0;
+}
