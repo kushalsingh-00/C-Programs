719509e34d88a480b605a1385917bc6bcefb2d7a
kushal Singh Rathore <54983775+kushalsingh-00@users.noreply.github.com>
2019-12-28 22:27:41 +0530

    we cannot assign value of variable to itself if we do so garbage value printed

---
+++ Problem3.c
@@ -0,0 +1,16 @@
+/*
+-------------------------------------------------------
+Name         : Problem 3
+Author       : Kushal Singh Rathore
+Discription  : Problem Solution 
+-------------------------------------------------------
+*/
+
+#include<stdio.h>
+int var=5;
+int main()
+{
+    int var=var;        
+    printf("%d",var);
+    return 0;
+}
