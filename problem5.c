f607c8caa4248fb3989a9c38d015c30d5a96c9cc
kushal Singh Rathore <54983775+kushalsingh-00@users.noreply.github.com>
2019-12-29 18:42:37 +0530

    problem basded on comma operator

---
+++ problem5.c
@@ -0,0 +1,17 @@
+/*
+-------------------------------------------------------
+Name         : Problem 3
+Author       : Kushal Singh Rathore
+Discription  : Problem Solution 
+-------------------------------------------------------
+*/
+
+#include<stdio.h>
+int main()
+{
+    int var;
+    int num;
+    num=(var=15,var+35);       
+    printf("%d",num);
+    return 0;
+}
