c55ad64c9a9989f46120fb38dc0abe6e70595107
kushal Singh Rathore <54983775+kushalsingh-00@users.noreply.github.com>
2019-12-29 18:12:21 +0530

    outer printf prints no of character and this is illustration of short hand operator

---
+++ problem4.c
@@ -0,0 +1,17 @@
+/*
+-------------------------------------------------------
+Name         : Problem 4
+Author       : Kushal Singh Rathore
+Discription  : Problem Solution 
+-------------------------------------------------------
+*/
+
+#include<stdio.h>
+int var=5;
+int main()
+{
+    char a=7;
+    a^=5;     
+    printf("%d",printf("%d",a+=3));              // it print first printf inside printf and then outer 
+    return 0;
+}
