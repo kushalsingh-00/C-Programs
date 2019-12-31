a5d3edc1b75672cd90e7698fec7a304487f0e4e1
kushal Singh Rathore <54983775+kushalsingh-00@users.noreply.github.com>
2019-12-31 10:19:35 +0530

    print one only

---
+++ problem9.c
@@ -0,0 +1,20 @@
+/*
+-------------------------------------------------------
+Name         : Problem 9
+Author       : Kushal Singh Rathore
+Discription  : Problem Solution 
+-------------------------------------------------------
+*/
+
+#include<stdio.h>
+int main()
+{
+    int i=0;
+    for(printf("one\n");i<3&&printf("");i++)     // false
+    {
+        printf("Hi!\n");
+    }
+    return 0;
+
+    
+}
