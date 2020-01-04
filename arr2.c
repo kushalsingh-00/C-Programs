be193176958b67c5b73f731265359200f754a88e
kushal Singh Rathore <54983775+kushalsingh-00@users.noreply.github.com>
2020-01-04 15:58:47 +0530

    it makes simple for  insert at some random places and put 0 for other places which are not specified

--- arr2.c
+++ arr2.c
@@ -9,10 +9,16 @@ Discription  : Put Values At Random Places
 #include<stdio.h>
 int main()
 {
-    int arr[10]={[0]=1,[5]=2,[4]=33};
-    for(int i=0;i<10;i++)
+    int arr[10]={[0]=1,[5]=2,[4]=33};  // 0,5,4 are designators
+    int a[]={1,7,4,[2]=90,6,[8]=9};    // higher value will be the size n+1
+    for(int i=0;i<10;i++)    //if designator and index have same value then designator will taken
     {
         printf("%d ",arr[i]);
+    }
+    printf("\n");
+    for(int i=0;i<10;i++)
+    {
+        printf("%d ",a[i]);
     }
     return 0;
 }
