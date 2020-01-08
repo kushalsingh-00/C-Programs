eb65a4bfcf0fb7fc0af2650eacef21f5ea89279b
kushal Singh Rathore <54983775+kushalsingh-00@users.noreply.github.com>
2020-01-08 19:01:39 +0530

    solution of a problem

---
+++ p7.c
@@ -0,0 +1,23 @@
+/*
+-------------------------------------------------------
+Name         : Pointers
+Author       : Kushal Singh Rathore
+Discription  : problem
+-------------------------------------------------------
+*/
+
+#include<stdio.h>
+int main()
+{
+int a[]={5,16,7,89,45,32,23,10};
+int *p=&a[1],*q=&a[5];
+printf("%d ",*(p+3));   
+printf("%d ",*(q-3));
+printf("%d ",q-p);
+printf("%d ",p<q);
+printf("%d ",*p<*q);
+return 0; 
+
+
+
+}
