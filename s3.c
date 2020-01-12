bf8e9994953c1c2a0fe954396d912fdbe6c402a5
kushal Singh Rathore <54983775+kushalsingh-00@users.noreply.github.com>
2020-01-12 19:28:01 +0530

    assigning string literal to a pointer

---
+++ s3.c
@@ -0,0 +1,19 @@
+/*
+-------------------------------------------------------
+Name         : String Literals
+Author       : Kushal Singh Rathore
+Discription  : Operations On String Literals
+-------------------------------------------------------
+*/
+
+#include<stdio.h>
+int main()
+{
+    char *p="hello";     //string literal is treated as pointer to the first character
+   // *p='m';      // we cannot change the value as string literal are string constant(read only memory)
+    printf("%c/n",*p); //example
+    printf("%c/n",*(p+1));  //example 
+
+
+    return 0;
+}
