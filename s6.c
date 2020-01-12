9f4ad21953cde96950970397f2d0da2d1edf1610
kushal Singh Rathore <54983775+kushalsingh-00@users.noreply.github.com>
2020-01-12 22:41:51 +0530

    scanf function

---
+++ s6.c
@@ -0,0 +1,19 @@
+/*
+-------------------------------------------------------
+Name         : String  
+Author       : Kushal Singh Rathore
+Discription  : Different Types Of Scanning Func
+-------------------------------------------------------
+*/
+
+#include<stdio.h>
+int main()
+{
+    char a[10];
+    char b[10];
+    printf("Enter the string:"); //9 in scanf store only nine char
+    scanf("%9s",a);  // there is no need to put & because as a is treated as pointer to first char and is itself address
+    printf("%s",a); // scanf does not store white space and read until a white
+    
+    return 0;
+}
