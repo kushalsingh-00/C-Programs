ad8afd5a8f1bbc9d54ae6969b0b35b019f75cc45
kushal Singh Rathore <54983775+kushalsingh-00@users.noreply.github.com>
2020-01-12 18:53:34 +0530

    %s is used for represent string

---
+++ s1.c
@@ -0,0 +1,20 @@
+/*
+-------------------------------------------------------
+Name         : String 
+Author       : Kushal Singh Rathore
+Discription  : Printf In Printf With String
+-------------------------------------------------------
+*/
+
+#include<stdio.h>
+int main()
+{
+    printf(" %d",printf("%s","Hello World"));   //%s also count and return length of words which it printed
+
+
+    printf("\n%10s","hello");    //for printing white spaces 10 used
+
+    return 0;
+}
+
+
