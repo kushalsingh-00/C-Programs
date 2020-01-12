85c63d66ff95ef0ef7cdacdd200877e798e39960
kushal Singh Rathore <54983775+kushalsingh-00@users.noreply.github.com>
2020-01-12 19:01:17 +0530

    splicing and its use

---
+++ s2.c
@@ -0,0 +1,19 @@
+/*
+-------------------------------------------------------
+Name         : String Splicing 
+Author       : Kushal Singh Rathore
+Discription  : Two Methods 
+-------------------------------------------------------
+*/
+
+#include<stdio.h>
+int main()
+{
+    printf("%s","playing with string.\
+    string are good");      // (backslace)\ is used for putting the string in next line 
+
+    printf("%s","playing with string."
+    "string are good"); // this is another method for same
+
+    //2nd one is prefered over first because first will add some empty spaces 
+}
