f8f35842b49e4088ba65a6b639da11a48b660829
kushal Singh Rathore <54983775+kushalsingh-00@users.noreply.github.com>
2020-01-13 19:19:55 +0530

    scanf function

--- s6.c
+++ s6.c
@@ -14,6 +14,8 @@ int main()
     printf("Enter the string:"); //9 in scanf store only nine char
     scanf("%9s",a);  // there is no need to put & because as a is treated as pointer to first char and is itself address
     printf("%s",a); // scanf does not store white space and read until a white
-    
+    getchar();      // here getchar is used because gets also store string and -
+    gets(b);        // and it store the enter after scanf and store it 
+    puts(b);
     return 0;
 }
