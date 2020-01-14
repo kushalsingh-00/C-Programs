d469f47cc25d68f4c8f08754ed89c8edc16bd916
kushal Singh Rathore <54983775+kushalsingh-00@users.noreply.github.com>
2020-01-14 09:25:47 +0530

    gate problem

---
+++ s14.c
@@ -0,0 +1,35 @@
+/*
+-------------------------------------------------------
+Name         : String  
+Author       : Kushal Singh Rathore
+Discription  : String Problem
+-------------------------------------------------------
+*/
+
+#include<stdio.h>
+#include<string.h>
+void fun1(char *s1,char *s2)
+{
+    char *temp;
+    temp=s1;
+    s1=s2;
+    s2=temp;
+}
+
+void fun2(char **s1,char **s2)
+{
+    char *temp;
+    temp=*s1;
+    *s1=*s2;
+    *s2=temp;
+}
+
+int main()
+{
+    char *str1="Hi",*str2="Bye";
+    fun1(str1,str2);
+    printf("%s %s",str1,str2);
+    fun2(&str1,&str2);
+    printf("%s %s",str1,str2);
+    return 0;
+}
