3574728f2f207c0a46bebb21ed08ac85be236272
kushal Singh Rathore <54983775+kushalsingh-00@users.noreply.github.com>
2020-01-14 09:39:52 +0530

    gate problem

---
+++ s15.c
@@ -0,0 +1,17 @@
+/*
+-------------------------------------------------------
+Name         : String  
+Author       : Kushal Singh Rathore
+Discription  : String Problem
+-------------------------------------------------------
+*/
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char *c="GATECSIT2019";
+    char *p=c;
+    printf("%d",(int)strlen(c+2[p]-6[p]-1));  //2[p]=*(2+p)
+    return 0;
+}
